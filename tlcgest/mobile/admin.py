4fd8d32f4b029da66d372290b3a2cd39c09b96bc
mauro-borg <mauro.borghi@gmail.com>
2020-02-20 23:27:47 +0000

    register Mobile devices in admin page

--- tlcgest/mobile/admin.py
+++ tlcgest/mobile/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Sim
+from .models import Sim, MobileDevice
 
 admin.site.register(Sim)
+admin.site.register(MobileDevice)
 
